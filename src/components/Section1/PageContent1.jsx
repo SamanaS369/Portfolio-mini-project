35e522b90b78ff7fa24cb016c6524d3cf947c45b
SamanaS369 <sthsamana2062@gmail.com>
2026-09-26 08:20:44 +0545

    logo

--- src/components/Section1/PageContent1.jsx
+++ src/components/Section1/PageContent1.jsx
@@ -6,47 +6,47 @@ const PageContent1 = () => {
    const skills = [
     {
       name: "Angular",
-      logo: "/images/angular.png",
+      logo: "https://blog.e-saurio.com/content/images/2023/02/the-seo-guide-to-angular.png",
     },
     {
       name: "Next.js",
-      logo: "/images/nextjs.png",
+      logo: "https://th.bing.com/th/id/OIP.mdLT2ZK_3OlDwK2R-Q2UlQHaGp?w=197&h=180&c=7&r=0&o=7&pid=1.7&rm=3",
     },
     {
       name: "TypeScript",
-      logo: "/images/typescript.png",
+      logo: "https://th.bing.com/th/id/OIP.sUsMgZa-MozU0lcYT-Y5xQHaHa?w=170&h=180&c=7&r=0&o=7&pid=1.7&rm=3",
     },
     {
       name: "JavaScript",
-      logo: "/images/javascript.png",
+      logo: "https://th.bing.com/th/id/OIP.VgcDbZtvRThnSNbJyvtTvQHaHa?w=166&h=180&c=7&r=0&o=7&pid=1.7&rm=3",
     },
     {
       name: "Tailwind",
-      logo: "/images/tailwind.png",
+      logo: "https://th.bing.com/th/id/OIP.22tPt9bi1FRyjOYQI8irPQHaD4?w=271&h=180&c=7&r=0&o=7&pid=1.7&rm=3",
     },
     {
       name: "Redux",
-      logo: "/images/redux.png",
+      logo: "https://th.bing.com/th/id/OIP.fMCFksfCUgQGWL3zS_ZulwHaHD?w=152&h=180&c=7&r=0&o=7&pid=1.7&rm=3",
     },
     {
       name: "Node.js",
-      logo: "/images/nodejs.png",
+      logo: "https://th.bing.com/th/id/OIP.GzwNcLewBwyBkK1mBCxoPQHaHa?w=152&h=180&c=7&r=0&o=7&pid=1.7&rm=3",
     },
     {
       name: "Express",
-      logo: "/images/express.png",
+      logo: "https://www.cisworld.lk/storage/technologies/express-js-1.jpg",
     },
     {
       name: "Python",
-      logo: "/images/python.png",
+      logo: "https://th.bing.com/th/id/OIP.xeoinwuNrrPBMdn8mcm7NwHaHa?w=154&h=180&c=7&r=0&o=7&pid=1.7&rm=3",
     },
     {
       name: "NestJS",
-      logo: "/images/nestjs.png",
+      logo: "https://th.bing.com/th/id/OIP.x4F99xzUMDw89anfCyLGxwHaD-?w=330&h=180&c=7&r=0&o=7&pid=1.7&rm=3",
     },
     {
       name: "React",
-      logo: "/images/react.png",
+      logo: "https://th.bing.com/th/id/OIP.eBnSJTFfIBedYR4lU_x16gHaGl?w=169&h=180&c=7&r=0&o=7&pid=1.7&rm=3",
     },
   ];
 
--- src/components/Section2/Section2.jsx
+++ src/components/Section2/Section2.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 const Section2 = () => {
   return (  
-       <div className='h-screen w-full bg-blue-100'>
+     <div className='h-screen w-full bg-blue-100'>
+<div className="absolute left-10 top-12 h-5 w-5 rounded-full bg-blue-300/60" />
+<div className="absolute left-20 top-24 h-8 w-8 rounded-full bg-blue-300/50" />
+      <div className="absolute right-20 top-20 h-10 w-10 rounded-full bg-blue-300/50" />
+      <div className="absolute bottom-10 left-1/3 h-12 w-12 rounded-full bg-blue-400/40" />
+      <div className="absolute right-10 bottom-20 h-7 w-7 rounded-full bg-blue-400/50" />
+</div>
+ 
       
-      
-    </div>
   )
 }
 
